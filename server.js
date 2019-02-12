b5ee53caf6727c13f1f029f267e4909dabf82b3b
stbaeumer <stefan.baeumer@berufskolleg-borken.de>
2019-02-12 13:29:26 +0100

    ...

---
+++ server.js
@@ -0,0 +1,9 @@
+// Das ist ein einzeiliger Kommentar
+/* Das ist ein mehrzeiliger Kommentar */
+
+// Der bodyparser bereitet die Daten aus dem HTML-Formular 
+// vor der Übergabe an die server.js auf.
+// Der bodyparser muss über das Terminal installiert werden:
+// npm install body-parser --save
+
+const bodyparser = require('body-parser')
