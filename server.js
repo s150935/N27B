4435e4f8f04a98b9db246e5e0ed0103709758c20
Stu <stbaeumer@gmail.com>
2019-03-19 08:28:22 +0100

    ...

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
-// Das ist ein einzeiliger Kommentar
-/* Das ist ein mehrzeiliger Kommentar */
-
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const cookieParser = require('cookie-parser')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
 app.get('/',(req, res, next) => {   
-    res.render('index.ejs', {                    
+
+    let idKunde = req.cookies['istAngemeldetAls']
+    
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('index.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
+
+app.get('/login',(req, res, next) => {         
+    res.cookie('istAngemeldetAls', '')       
+    res.render('login.ejs', {                    
     })
 })
 
+app.post('/',(req, res, next) => {   
+    
+    const idKunde = req.body.idKunde
+    const kennwort = req.body.kennwort
+        
+    if(idKunde === "4711" && kennwort === "123"){            
+        console.log("Der Cookie wird gesetzt:")
+        res.cookie('istAngemeldetAls', idKunde)
+        res.render('index.ejs', {           
+        })
+    }else{            
+        console.log("Der Cookie wird gelöscht")
+        res.cookie('istAngemeldetAls','')
+        res.render('login.ejs', {                    
+        })
+    }
+})
